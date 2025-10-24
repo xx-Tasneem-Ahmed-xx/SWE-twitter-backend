5a0a27af90418f2608ce39d994647fcda218664e
Tasneem Ahmed <tasneem.mostafa04@eng-st.cu.edu.eg>
2025-10-24 09:49:40 +0300

    feat: search by latest

--- src/application/dtos/tweets/tweet.dto.ts
+++ src/application/dtos/tweets/tweet.dto.ts
@@ -1,9 +1,12 @@
 import z from "zod";
 import {
   CreateTweetDTOSchema,
+  SearchDTOSchema,
   TimelineSchema,
 } from "@/application/dtos/tweets/tweet.dto.schema";
 
 export type CreateTweetDTO = z.infer<typeof CreateTweetDTOSchema>;
 
 export type TimlineDTO = z.infer<typeof TimelineSchema>;
+
+export type searchDTO = z.infer<typeof SearchDTOSchema>;
