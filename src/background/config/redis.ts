390599787aae13ebd2d3fe00806b31f42b9e7a19
Ahmed Hazem <149868137+ahmed-haz@users.noreply.github.com>
2025-11-22 11:23:21 +0200

    Update redis.ts

--- src/background/config/redis.ts
+++ src/background/config/redis.ts
@@ -1,12 +1,15 @@
 import { RedisOptions } from "ioredis";
-import { getKey } from "@/application/services/secrets";
+//import { getKey } from "@/application/services/secrets";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 let bullRedisConfig: RedisOptions;
 
 export async function initBullRedisConfig(): Promise<void> {
-  const host = await getKey("BULLMQ_REDIS_URL");
-  const port = await getKey("REDIS_PORT");
-  const password = await getKey("REDIS_PASSWORD");
+  const host = process.env.BULLMQ_REDIS_URL
+  const port = process.env.REDIS_PORT;
+  const password = process.env.REDIS_PASSWORD;
 
   bullRedisConfig = {
     host,
